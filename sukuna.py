4ffa87b6922bb550fb33a7b01aa9df7582c3ddd6
alphabet9470 <worknshubh@outlook.com>
2023-10-23 10:14:37 -0400

    updated sukuna.py

--- sukuna.py
+++ sukuna.py
@@ -74,7 +74,7 @@ prRed("Know Your Place Fool...")
 time.sleep(1)
 command_output = subprocess.getoutput('sudo /usr/sbin/airmon-ng')
 interface_name = command_output.split()
-user_input = int(input("\nPress (1)  Monitor Mode \nPress (2) for Managed Mode\nPress (3) For Scan\nPress (4) For Crack\n>>>"))
+user_input = int(input("\nPress (1) For Monitor Mode \nPress (2) For Managed Mode\nPress (3) For Scan\nPress (4) For Crack\nPress (5) For Exit\n>>>"))
 if user_input == 1:
     command_1 = "sudo airmon-ng start "+interface_name[5]
     subprocess.run(command_1,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
@@ -82,7 +82,7 @@ if user_input == 1:
 
 elif user_input==3:
     scanner()
-elif user_input==2:
+elif user_input==4:
     target_cap = input("Enter the last number of the Handshake [e.g: 01]\n>>>")
     command_6 = ["sudo", "aircrack-ng", "-w", "wordlist.txt", f"targets/target_wifi-{target_cap}.cap"]
     subprocess.run(command_6)
@@ -90,7 +90,7 @@ elif user_input == 2:
     command_2 = "sudo airmon-ng stop "+interface_name[5]
     subprocess.run(command_2,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
     prGreen("Managed Mode has been Enabled")
-elif user_input==3:
+elif user_input==5:
     prYellow("\nExiting the Program!")
 else:
     prRed("You Tried to be Oversmart \n Exiting the Program")
